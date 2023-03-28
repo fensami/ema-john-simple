c30d2e4ec24700ad46f991dc9a68f5fd8c3b5d59
fensami <113832498+fensami@users.noreply.github.com>
2023-03-28 11:45:35 +0600

    added grand totol price

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,20 +5,24 @@ const Cart = ({cart}) => { //option-3
     // const cart = props.cart;//option-1
     // const {cart} = props; //option-2
 
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for (const product of cart){
-        total = total + product.price
+        totalPrice = totalPrice + product.price
+        totalShipping = totalShipping + product.shipping
     }
+    let tax = totalPrice * 7 /100
+    let grandTotal = totalPrice + totalShipping + tax
 
    
     return (
         <div className='cart'>
             <h2>show cart</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total price : {total} </p>
-            <p>Total Shipping : </p>
-            <p>Tax: </p>
-            <h6>Grand Total</h6>
+            <p>Total price : ${totalPrice} </p>
+            <p>Total Shipping : {totalShipping} </p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <h4>Grand Total: $ {grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
